ef8b371adda9aa8e59607411b5ce5d74c2b89d5c
Woobeen Jeon <woobeen0516@gmail.com>
2025-09-07 20:04:55 +0900

    리팩토링

--- core/src/main/kotlin/com/bindglam/neko/listeners/ItemListener.kt
+++ core/src/main/kotlin/com/bindglam/neko/listeners/ItemListener.kt
@@ -3,6 +3,8 @@ package com.bindglam.neko.listeners
 import com.bindglam.neko.api.NekoProvider
 import com.bindglam.neko.api.content.item.block.CustomBlock
 import com.bindglam.neko.utils.isReplaceable
+import org.bukkit.GameEvent
+import org.bukkit.GameMode
 import org.bukkit.Location
 import org.bukkit.block.Block
 import org.bukkit.entity.Player
@@ -10,13 +12,15 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
+import java.util.function.Consumer
 
 class ItemListener : Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun PlayerInteractEvent.tryUseCustomBlock() {
         clickedBlock ?: return
-        if(action.isLeftClick) return
 
         NekoProvider.neko().contentManager().customBlock(clickedBlock!!) ?: return
 
@@ -36,12 +40,13 @@ class ItemListener : Listener {
         val location = if(!clickedBlock!!.type.isReplaceable) clickedBlock!!.getRelative(blockFace).location else clickedBlock!!.location
 
         if(player.canPlaceBlock(location)) {
-            location.block.type = item!!.type
-            player.swingHand(hand!!)
+            isCancelled = true
+
+            player.placeBlock(location, { it.block.type = item!!.type }, clickedBlock!!, hand!!)
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     fun PlayerInteractEvent.tryPlaceCustomBlock() {
         item ?: return
         hand ?: return
@@ -56,11 +61,32 @@ class ItemListener : Listener {
         val location = if(!clickedBlock!!.type.isReplaceable) clickedBlock!!.getRelative(blockFace).location else clickedBlock!!.location
 
         if(player.canPlaceBlock(location)) {
-            customBlock.mechanism().place(location)
-            player.swingHand(hand!!)
-
             isCancelled = true
+
+            player.placeBlock(location, { customBlock.mechanism().place(it) }, clickedBlock!!, hand!!)
         }
+    }
+
+    private fun Player.placeBlock(location: Location, placeAction: Consumer<Location>, placedAgainst: Block, hand: EquipmentSlot) {
+        val item = inventory.getItem(hand)
+
+        val prevBlockData = location.block.blockData.clone()
+
+        placeAction.accept(location)
+        swingHand(hand)
+
+        val event = BlockPlaceEvent(location.block, location.block.state, placedAgainst, item, this, true, hand)
+        event.callEvent()
+
+        if(event.isCancelled || !event.canBuild()) {
+            location.block.blockData = prevBlockData
+            return
+        }
+
+        if(gameMode != GameMode.CREATIVE)
+            item.amount -= 1
+
+        location.world.sendGameEvent(player, GameEvent.BLOCK_PLACE, location.toVector())
     }
 
     private fun Block.isInteractable(): Boolean = NekoProvider.neko().contentManager().customBlock(this) == null && type.isInteractable
