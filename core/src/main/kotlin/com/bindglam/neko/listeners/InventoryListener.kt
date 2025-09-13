94183d094e83e6c67247d5f7fe3a34ac57e1878e
Woobeen Jeon <woobeen0516@gmail.com>
2025-09-13 21:30:02 +0900

    커스텀 아이템으로 마인크래프트 아이템을 제작할 수 있는 버그 진짜 수정(#8)

--- core/src/main/kotlin/com/bindglam/neko/listeners/InventoryListener.kt
+++ core/src/main/kotlin/com/bindglam/neko/listeners/InventoryListener.kt
@@ -1,25 +1,41 @@
 package com.bindglam.neko.listeners
 
 import com.bindglam.neko.api.NekoProvider
-import net.kyori.adventure.key.Key
-import net.kyori.adventure.key.Keyed
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.PrepareItemCraftEvent
+import org.bukkit.inventory.CookingRecipe
+import org.bukkit.inventory.RecipeChoice
+import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.inventory.ShapelessRecipe
+import org.bukkit.inventory.SmithingRecipe
+import org.bukkit.inventory.SmithingTransformRecipe
+import org.bukkit.inventory.SmithingTrimRecipe
+import org.bukkit.inventory.StonecuttingRecipe
+import org.bukkit.inventory.TransmuteRecipe
 
 class InventoryListener : Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     fun PrepareItemCraftEvent.cancelCraftItemsWithCustomItems() {
         recipe ?: return
-        if(recipe !is Keyed) return
-
-        val key = (recipe as Keyed).key()
+        val recipe = recipe!!
 
         val useCustomItem = inventory.matrix.any { it != null && NekoProvider.neko().contentManager().customItem(it) != null }
         if(!useCustomItem) return
 
-        if(key.namespace() == Key.MINECRAFT_NAMESPACE)
-            inventory.result = null
+        when(recipe) {
+            is ShapedRecipe -> recipe.choiceMap.forEach { ch, choice -> if(choice is RecipeChoice.MaterialChoice) inventory.result = null }
+            is ShapelessRecipe -> recipe.choiceList.forEach { choice -> if(choice is RecipeChoice.MaterialChoice) inventory.result = null }
+            is TransmuteRecipe -> if(recipe.input is RecipeChoice.MaterialChoice || recipe.material is RecipeChoice.MaterialChoice) inventory.result = null
+
+            is SmithingTransformRecipe -> if(recipe.base is RecipeChoice.MaterialChoice || recipe.addition is RecipeChoice.MaterialChoice || recipe.template is RecipeChoice.MaterialChoice) inventory.result = null
+            is SmithingTrimRecipe -> if(recipe.base is RecipeChoice.MaterialChoice || recipe.addition is RecipeChoice.MaterialChoice || recipe.template is RecipeChoice.MaterialChoice) inventory.result = null
+            is SmithingRecipe -> if(recipe.base is RecipeChoice.MaterialChoice || recipe.addition is RecipeChoice.MaterialChoice) inventory.result = null
+
+            is StonecuttingRecipe -> if(recipe.inputChoice is RecipeChoice.MaterialChoice) inventory.result = null
+
+            is CookingRecipe<*> -> if(recipe.inputChoice is RecipeChoice.MaterialChoice) inventory.result = null
+        }
     }
 }
