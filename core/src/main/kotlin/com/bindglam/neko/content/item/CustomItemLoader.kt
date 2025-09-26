2c2fed8ff1b014c2b5b506d0af5d8a4b209f0b32
Woobeen Jeon <woobeen0516@gmail.com>
2025-09-26 16:15:01 +0900

    리팩토링

--- core/src/main/kotlin/com/bindglam/neko/content/item/CustomItemLoader.kt
+++ core/src/main/kotlin/com/bindglam/neko/content/item/CustomItemLoader.kt
@@ -10,7 +10,9 @@ import org.bukkit.configuration.ConfigurationSection
 
 class CustomItemLoader : ContentLoader {
     override fun load(key: Key, config: ConfigurationSection){
-        BuiltInRegistries.ITEMS.register(key, CustomItem(NamespacedKey(key.namespace(), key.value()), CUSTOM_ITEM_PROPERTIES_CONFIGURABLE.load(config.getConfigurationSection("properties.item"))!!))
+        CustomItem(NamespacedKey(key.namespace(), key.value()), CUSTOM_ITEM_PROPERTIES_CONFIGURABLE.load(config.getConfigurationSection("properties.item"))!!).also {
+            BuiltInRegistries.ITEMS.register(key, it)
+        }
     }
 
     override fun id(): String = "item"
