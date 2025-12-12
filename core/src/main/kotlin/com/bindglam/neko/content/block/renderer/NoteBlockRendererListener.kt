7db3a28e194f0a99aae54141251895c093492b5d
Woobeen Jeon <woobeen0516@gmail.com>
2025-12-12 19:41:35 +0900

    리팩토링

--- core/src/main/kotlin/com/bindglam/neko/content/block/renderer/NoteBlockRendererListener.kt
+++ core/src/main/kotlin/com/bindglam/neko/content/block/renderer/NoteBlockRendererListener.kt
@@ -6,6 +6,7 @@ import org.bukkit.Instrument
 import org.bukkit.Material
 import org.bukkit.Note
 import org.bukkit.Tag
+import org.bukkit.block.Block
 import org.bukkit.block.data.type.NoteBlock
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
@@ -32,36 +33,7 @@ class NoteBlockRendererListener : Listener {
         } else {
             val belowBlock = block.world.getBlockAt(block.location.add(0.0, -1.0, 0.0))
 
-            if(Tag.OAK_LOGS.isTagged(belowBlock.type))
-                instrument = Instrument.BASS_GUITAR
-            else if(belowBlock.type == Material.SAND || belowBlock.type == Material.GRAVEL)
-                instrument = Instrument.SNARE_DRUM
-            else if(belowBlock.type == Material.GLASS)
-                instrument = Instrument.STICKS
-            else if(belowBlock.type == Material.STONE)
-                instrument = Instrument.BASS_DRUM
-            else if(belowBlock.type == Material.GOLD_BLOCK)
-                instrument = Instrument.BELL
-            else if(belowBlock.type == Material.CLAY)
-                instrument = Instrument.FLUTE
-            else if(belowBlock.type == Material.PACKED_ICE)
-                instrument = Instrument.CHIME
-            else if(Tag.WOOL.isTagged(belowBlock.type))
-                instrument = Instrument.GUITAR
-            else if(belowBlock.type == Material.BONE_BLOCK)
-                instrument = Instrument.XYLOPHONE
-            else if(belowBlock.type == Material.IRON_BLOCK)
-                instrument = Instrument.IRON_XYLOPHONE
-            else if(belowBlock.type == Material.SOUL_SAND)
-                instrument = Instrument.COW_BELL
-            else if(belowBlock.type == Material.PUMPKIN)
-                instrument = Instrument.DIDGERIDOO
-            else if(belowBlock.type == Material.EMERALD_BLOCK)
-                instrument = Instrument.BIT
-            else if(belowBlock.type == Material.HAY_BLOCK)
-                instrument = Instrument.BANJO
-            else if(belowBlock.type == Material.GLOWSTONE)
-                instrument = Instrument.PLING
+            instrument = getInstrument(belowBlock)
 
             block.blockData = (block.blockData as NoteBlock).also {
                 it.instrument = VanillaInstruments.HARP.bukkit
@@ -69,5 +41,40 @@ class NoteBlockRendererListener : Listener {
                 it.isPowered = false
             }
         }
+    }
+
+    private fun getInstrument(belowBlock: Block): Instrument {
+        return if(Tag.OAK_LOGS.isTagged(belowBlock.type))
+            Instrument.BASS_GUITAR
+        else if(belowBlock.type == Material.SAND || belowBlock.type == Material.GRAVEL)
+            Instrument.SNARE_DRUM
+        else if(belowBlock.type == Material.GLASS)
+            Instrument.STICKS
+        else if(belowBlock.type == Material.STONE)
+            Instrument.BASS_DRUM
+        else if(belowBlock.type == Material.GOLD_BLOCK)
+            Instrument.BELL
+        else if(belowBlock.type == Material.CLAY)
+            Instrument.FLUTE
+        else if(belowBlock.type == Material.PACKED_ICE)
+            Instrument.CHIME
+        else if(Tag.WOOL.isTagged(belowBlock.type))
+            Instrument.GUITAR
+        else if(belowBlock.type == Material.BONE_BLOCK)
+            Instrument.XYLOPHONE
+        else if(belowBlock.type == Material.IRON_BLOCK)
+            Instrument.IRON_XYLOPHONE
+        else if(belowBlock.type == Material.SOUL_SAND)
+            Instrument.COW_BELL
+        else if(belowBlock.type == Material.PUMPKIN)
+            Instrument.DIDGERIDOO
+        else if(belowBlock.type == Material.EMERALD_BLOCK)
+            Instrument.BIT
+        else if(belowBlock.type == Material.HAY_BLOCK)
+            Instrument.BANJO
+        else if(belowBlock.type == Material.GLOWSTONE)
+            Instrument.PLING
+        else
+            Instrument.PIANO
     }
 }
