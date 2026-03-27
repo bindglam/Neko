913d4d05c2106121f02a76b46230c590a50179ff
bindglam <woobeen0516@gmail.com>
2026-03-27 11:50:27 +0900

    refactor: AbstractContent

--- core/src/main/java/io/github/bindglam/neko/content/AbstractContent.java
+++ core/src/main/java/io/github/bindglam/neko/content/AbstractContent.java
@@ -2,34 +2,23 @@ package io.github.bindglam.neko.content;
 
 import io.github.bindglam.neko.content.feature.Feature;
 import io.github.bindglam.neko.content.feature.FeatureBuilder;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 import net.kyori.adventure.key.Key;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Unmodifiable;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public abstract class AbstractContent implements Content {
+    @Getter @Accessors(fluent = true)
     protected final Key key;
+    @Getter @Accessors(fluent = true)
     protected final List<Feature> features;
 
     protected AbstractContent(@NotNull Key key, @NotNull List<FeatureBuilder> features) {
         this.key = key;
-        this.features = Collections.unmodifiableList(
-                features.stream()
-                        .map(builder -> builder.build(this))
-                        .toList()
-        );
-    }
-
-    @Override
-    public @NotNull Key key() {
-        return key;
-    }
-
-    @Override
-    public @NotNull @Unmodifiable List<Feature> features() {
-        return features;
+        this.features = features.stream()
+                .map(builder -> builder.build(this))
+                .toList();
     }
 }
