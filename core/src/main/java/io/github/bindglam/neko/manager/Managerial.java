f542de77259687b65920b8575430ae667e23f031
bindglam <woobeen0516@gmail.com>
2026-03-27 11:57:59 +0900

    refactor: Managerial

--- core/src/main/java/io/github/bindglam/neko/manager/Managerial.java
+++ core/src/main/java/io/github/bindglam/neko/manager/Managerial.java
@@ -1,9 +1,11 @@
 package io.github.bindglam.neko.manager;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface Managerial {
-    void preload(Context context);
+    void preload(@NotNull Context context);
 
-    void start(Context context);
+    void start(@NotNull Context context);
 
-    void end(Context context);
+    void end(@NotNull Context context);
 }
