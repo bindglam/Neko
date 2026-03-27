6cc9e873afbf349996c7abdfe1d88f9f7c131342
bindglam <woobeen0516@gmail.com>
2026-03-27 11:57:55 +0900

    refactor: Context

--- core/src/main/java/io/github/bindglam/neko/manager/Context.java
+++ core/src/main/java/io/github/bindglam/neko/manager/Context.java
@@ -3,14 +3,7 @@ package io.github.bindglam.neko.manager;
 import io.github.bindglam.neko.NekoPluginImpl;
 import org.jetbrains.annotations.NotNull;
 
-public final class Context {
-    private final NekoPluginImpl plugin;
-
-    public Context(@NotNull NekoPluginImpl plugin) {
-        this.plugin = plugin;
-    }
-
-    public NekoPluginImpl plugin() {
-        return plugin;
-    }
+public record Context(
+        @NotNull NekoPluginImpl plugin
+) {
 }
